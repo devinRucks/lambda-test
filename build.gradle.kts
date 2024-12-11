a173cbaa078936e9835ab9590d839d1df0d0b873
devinrucks <drucks@warrenrogers.com>
2024-12-10 22:17:41 -0500

    ADD buildspec and template

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,13 @@ repositories {
 }
 
 dependencies {
+//    implementation("com.amazonaws:aws-lambda-java-core:1.2.1")
+//    implementation("com.amazonaws:aws-lambda-java-events:2.2.7")
+//    implementation("com.amazonaws:aws-java-sdk-lambda:1.11.762")
+
     implementation("com.amazonaws:aws-java-sdk-lambda:1.11.762")
+    implementation("com.amazonaws:aws-lambda-java-events:2.2.7")
+    implementation("com.amazonaws:aws-lambda-java-core:1.2.0")
     testImplementation(kotlin("test"))
 }
 
--- src/main/kotlin/LambdaEntry.kt
+++ src/main/kotlin/LambdaEntry.kt
@@ -1,6 +1,9 @@
+import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.amazonaws.services.lambda.runtime.Context
 
 
 class LambdaEntry : RequestHandler<Map<String, Any>, String> {
+
     override fun handleRequest(input: Map<String, Any>, context: Context): String {
         context.logger.log("Input received: $input")
         return "Hello, ${input["name"] ?: "World"}!"
