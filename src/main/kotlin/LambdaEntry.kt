c50b79a0db8957a59b43a8ab8663ad94995d8adf
devinrucks <drucks@warrenrogers.com>
2025-01-31 13:57:13 -0500

    UPDATE return string

--- src/main/kotlin/LambdaEntry.kt
+++ src/main/kotlin/LambdaEntry.kt
@@ -3,19 +3,9 @@ import com.amazonaws.services.lambda.runtime.Context
 
 
 class LambdaEntry : RequestHandler<Map<String, Any>, String> {
-    val testFile = TestFile()
-
-    init {
-        println(this.javaClass.name)
-    }
 
     override fun handleRequest(input: Map<String, Any>, context: Context): String {
         context.logger.log("Input received: $input")
-        val tests = mutableListOf<Test>()
-        for (i in (1..50)) {
-            tests.add(Test(i.toString()))
-        }
-        println(tests)
-        return "Hello, World!"
+        return "Hello, World, this is a test!"
     }
 }
--- src/main/kotlin/TestFile.kt
+++
@@ -1,8 +0,0 @@
-class TestFile {
-    init {
-        val tests = mutableListOf<Test>()
-        for (i in (1..400)) {
-            tests.add(Test(i.toString()))
-        }
-    }
-}
